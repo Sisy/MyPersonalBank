a3adea58ca44971626c035956a24fc3cef3af664
Cory <cory@alum.wpi.edu>
2014-10-26 21:31:23 -0400

    Converting to gradle project using Android Studio

---
+++ app/src/main/java/com/example/personalbank/ExpenseTrackerAdapter.java
@@ -0,0 +1,80 @@
+package com.example.personalbank;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+public class ExpenseTrackerAdapter extends BaseAdapter{
+    private ArrayList<ExpenseLogEntryData> arrayExpenseLog = new ArrayList<ExpenseLogEntryData>();
+    private Context context;
+    private TextView vHead;
+    private TextView vExpense;
+    private TextView vDate;
+
+    ExpenseTrackerAdapter(){
+
+    }
+
+    ExpenseTrackerAdapter(Context context, ArrayList<ExpenseLogEntryData> array){
+        this.context = context;
+        this.arrayExpenseLog = array;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return arrayExpenseLog.size();
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+    @Override
+    public ExpenseLogEntryData getItem(int index) {
+        // TODO Auto-generated method stub
+        return arrayExpenseLog.get(index);
+    }
+
+    @Override
+    public long getItemId(int index) {
+        // TODO Auto-generated method stub
+        return index;
+    }
+
+    @Override
+    public View getView(int index, View convertView, ViewGroup parent) {
+        ExpenseLogEntryData expenseData = getItem(index);
+
+        if (convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            convertView = inflater.inflate(R.layout.expense_entry, null);
+        }
+        vHead = (TextView) convertView.findViewById(R.id.heading);
+        vExpense = (TextView) convertView.findViewById(R.id.expense);
+        vDate = (TextView) convertView.findViewById(R.id.date);
+
+        vHead.setText(expenseData.getHeading());
+        vExpense.setText(expenseData.getExpense());
+        vDate.setText(expenseData.getDate().toString());
+
+        return convertView;
+    }
+
+    public void add(ExpenseLogEntryData data){
+        arrayExpenseLog.add(data);
+
+    }
+
+    public void remove(int index){
+        arrayExpenseLog.remove(index);
+    }
+
+}
+
---
+++ app/src/main/java/com/example/personalbank/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.personalbank;
+/*This is a personal bank. It is used to record daily spend .
+ * Run the application. Then using menu add function to add entry. Using SQLite to store data.*/
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import java.util.*;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import android.widget.AdapterView.OnItemClickListener;
+
+@SuppressLint("NewApi")
+public class MainActivity extends ListActivity {
+    ListView listView;
+
+    String newHead;
+    String newNote;
+    ExpenseLogDataSource dataSource;
+    SimpleCursorAdapter adapter;
+    Button btnDelete;
+    Cursor mCursor;
+
+    @SuppressLint("InlinedApi")
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = getListView();
+
+
+        dataSource = new ExpenseLogDataSource(this);
+        dataSource.open();
+
+        mCursor = dataSource.getAllExpense();
+        startManagingCursor(mCursor);
+        adapter = new SimpleCursorAdapter(
+                this,
+                R.layout.expense_entry,
+                mCursor,
+                new String[]{MySQLiteHelper.COLUMN_HEAD,MySQLiteHelper.COLUMN_NOTE,MySQLiteHelper.COLUMN_DATE},
+                new int[]{R.id.heading,R.id.expense,R.id.date},
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener(){
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+
+                Toast.makeText(getApplicationContext(),"Your consumption record on "+ mCursor.getString(3),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void onClickDelete(View v){
+        View parent = (View)v.getParent();//expense_entry.xml
+
+        final int position = listView.getPositionForView(parent);
+
+        Builder bd = new AlertDialog.Builder(this);
+        bd.setTitle("Delete Log");
+        bd.setMessage("Are you sure to delete this expense log?");
+        bd.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+
+                Cursor n = (Cursor)adapter.getItem(position);
+                long id = (long)n.getInt(0);
+
+                dataSource.deleteExpense(id);
+                mCursor = dataSource.getAllExpense();
+                adapter.swapCursor(mCursor);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        bd.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        bd.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                startActivityForResult(new Intent("com.example.personalbank.AddExpense"),1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 1){
+            if(resultCode == RESULT_OK){
+                Bundle extras = data.getExtras();
+                newHead = extras.getString("com.example.personalbank.heading");
+                newNote = extras.getString("com.example.personalbank.note");
+                ExpenseLogEntryData new_d = new ExpenseLogEntryData(newHead, newNote);
+                dataSource.insertExpense(new_d);
+                mCursor = dataSource.getAllExpense();
+                adapter.swapCursor(mCursor);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+    @Override
+    protected void onResume() {
+        dataSource.open();
+        super.onResume();
+    }
+
+
+
+}
+
